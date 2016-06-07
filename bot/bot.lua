271594069b781a5edc412592cd439916c2ec158c
ERMIA2580 <mohammad.b.mb72@gmail.com>
2016-06-07 10:27:31 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,15 +228,15 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {181282127},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
   print ('saved config into ./data/config.lua')
 end
 
-function on_our_id (id)
-  @ERER2580 = id
+function on_our_id (181282127)
+  @ERER2580 = 181282127
 end
 
 function on_user_update (user, what)
